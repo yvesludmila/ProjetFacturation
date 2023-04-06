e6774105615d3062974b3498afad6108dec6ae05
yves ludmila RAKOTOARIMALALA <105701277+yvesludmila@users.noreply.github.com>
2023-04-06 14:12:56 +0100

    intro-facturation

---
+++ src/Components/NavBar/NavBar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./NavBar.scss";
+const NavBar = () => {
+  return (
+    <div className="Nav">
+      <div className="Nav-menu">
+        <h3>Logo</h3>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
